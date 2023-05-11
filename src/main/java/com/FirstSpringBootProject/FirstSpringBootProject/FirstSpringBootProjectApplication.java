631dcda88f81fec8440701fb1edc2a2e8566d4f1
ke-stack <kingkevo254@gmail.com>
2023-05-11 22:37:46 +0300

    My initial Commit, hello world program

---
+++ src/main/java/com/FirstSpringBootProject/FirstSpringBootProject/FirstSpringBootProjectApplication.java
@@ -0,0 +1,22 @@
+package com.FirstSpringBootProject.FirstSpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstSpringBootProjectApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String greet() {
+		return "Hello";
+	}
+
+}
